3a5be698d98fdb7177b0b2869794b6c2ea452831
DenL <den.litviniuk@gmail.com>
2024-06-10 23:20:21 +0300

    new leetcode problems 10.06.2024

--- tests.py
+++ tests.py
@@ -1,9 +1,1 @@
-import math
-
-temperatures = [73,74,75,71,69,72,76,73]
-n = len(temperatures)
-result = [0 for _ in range(n)]
-
-for i, t in enumerate(reversed(temperatures)):
-    pass
-print(temperatures)
+ 
